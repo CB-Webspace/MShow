d4b7793cbe72d2eaa9506edda738179739bdc594
Brent Carroll <the.ironman.mark1@live.com>
2015-06-26 17:28:38 +0000

    added the working java file.

---
+++ Vol-log/php/send.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <script>
+            var name= promp("Hi, What is Your Name?")
+        </script>
+        <?php
+        $name = name
+        print $name
+        ?>
+    </body>
+</html>
